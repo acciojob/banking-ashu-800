51b0ebbd204599bc4daafd882e9d9130fe660d19
Ashutosh <ashushukla800@gmail.com>
2023-08-20 11:28:02 +0530

    second

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -4,6 +4,7 @@ public class StudentAccount extends BankAccount{
 
     String  institutionName;
 
+
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
         super(name,balance,0);
@@ -11,4 +12,11 @@ public class StudentAccount extends BankAccount{
 
     }
 
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
 }
